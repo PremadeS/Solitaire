f5d6802dd5ee0da02d506578c4cec4b457ceade1
PremadeS <128969155+PremadeS@users.noreply.github.com>
2025-04-03 16:14:56 +0500

    Add: gif background

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
-#include "game.h"
 #include <QApplication>
+#include <QLabel>
+#include <QMovie>
+#include <QWidget>
+
+#include "game.h"
 
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
@@ -7,9 +11,17 @@ int main(int argc, char *argv[]) {
   QWidget window;
   window.setFixedSize(1280, 720);
 
+  QLabel *backgroundLabel = new QLabel(&window);
+  backgroundLabel->setGeometry(0, 0, window.width(), window.height());
+
+  QMovie *movie = new QMovie("../assets/bg/bg.gif");
+  backgroundLabel->setMovie(movie);
+  movie->start();
+
   Game game(&window);
 
-  window.setStyleSheet("background-color: green");
+  window.setStyleSheet("background-color: green;");
+
   window.show();
 
   return a.exec();
